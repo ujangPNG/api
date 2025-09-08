0657921870760b5b79a12eb5f175610e26c90ce6
ujang <apasihsh@gmail.com>
2025-09-08 12:11:27 +0700

    s

--- spotify/script.js
+++ spotify/script.js
@@ -7,7 +7,7 @@ let userProfile = null;
 let currentLeaderboardData = null;
 
 // API Base URL - Update this to your Vercel deployment URL
-const API_BASE_URL ="https://spotify-muzmtry6j-ujangpngs-projects.vercel.app/api";
+const API_BASE_URL ="https://spotify-blsbf7osz-ujangpngs-projects.vercel.app/api";
 
 function toggleLanguage() {
     currentLanguage = currentLanguage === "id" ? "en" : "id";
